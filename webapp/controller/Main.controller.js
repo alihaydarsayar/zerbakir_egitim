460a352c80fe8eefc577c14f1dd983022e14d408
Ali Haydar Sayar <97897666+alihaydarsayar@users.noreply.github.com>
2025-09-03 18:18:08 +0300

    düzenlemeler v2

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -3,12 +3,14 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
     "sap/m/MessageBox",
+    "sap/ui/core/Fragment"
 ],
     function (
         Controller,
         JSONModel,
         MessageToast,
-        MessageBox
+        MessageBox,
+        Fragment
     ) {
         "use strict";
 
@@ -20,9 +22,28 @@ sap.ui.define([
                 var oViewJsonModel = new JSONModel(
                     {
                         busy: false,
+                        // createCustomer: {
+                        //     IvFirstName: "John",
+                        //     IvLastName: "Wick",
+                        //     IvEmail: "john.wick@com",
+                        //     IvPhone: "1",
+                        //     IvCity: "1",
+                        //     IvCountry: "tr"
+                        // }
                     }
                 );
                 this.getView().setModel(oViewJsonModel, "oMainViewModel");
+
+                // Müşteri oluşturma için model
+                var oCreateCustomerModel = new JSONModel({
+                    IvFirstName: "John",
+                    IvLastName: "Wick",
+                    IvEmail: "john.wick@com",
+                    IvPhone: "1",
+                    IvCity: "1",
+                    IvCountry: "tr"
+                });
+                this.getView().setModel(oCreateCustomerModel, "createCustomerModel");
 
                 this.onGetCustomerList();
 
@@ -31,12 +52,13 @@ sap.ui.define([
             onGetCustomerList: function () {
                 var that = this;
                 var oModel = this.getOwnerComponent().getModel();
-
                 // create
                 // delete
                 // uptade
                 // oModel.read("/GetCustomerListSet",{
                 // this.oDataModel.read("/GetCustomerListSet", {
+
+
                 oModel.read("/GetCustomerListSet", {
                     success: function (oData, oResponse) {
                         var oResults = oData.results;
@@ -47,26 +69,110 @@ sap.ui.define([
                         else {
                             MessageBox.warning("Veri Bulunamadı");
                         }
-                        that.getView().getModel("mainModel").setProperty("/customerList", oResults);
+                        this.getView().getModel("mainModel").setProperty("/customerList", oResults);
                         that.getView().getModel("oMainViewModel").setProperty("/customerList", oResults);
-                    },
+                    }.bind(this),
                     error: function (oError) {
-                     MessageBox.error("Hata alındı");
+                        MessageBox.error("Hata alındı");
                     }
                 })
 
             },
 
-            // onBeforeRendering: function(){
+            onCreateCustomer: function () {
+                var that = this;
 
-            // },
-            // onAfterRendering: function(){
+                // Model'i temizle
+                // this.getView().getModel("createCustomerModel").setData({
+                //     IvFirstName: "",
+                //     IvLastName: "",
+                //     IvEmail: "",
+                //     IvPhone: "",
+                //     IvCity: "",
+                //     // IvCountry: ""
+                // });
 
-            // },
-            // onExit: function(){
+                this.getView().getModel("createCustomerModel").setProperty("/IvFirstName", "");
+                this.getView().getModel("createCustomerModel").setProperty("/IvLastName", "");
+                this.getView().getModel("createCustomerModel").setProperty("/IvEmail", "");
+                this.getView().getModel("createCustomerModel").setProperty("/IvPhone", "");
+                this.getView().getModel("createCustomerModel").setProperty("/IvCity", "");
+                this.getView().getModel("createCustomerModel").setProperty("/IvCountry", "");
+                // this.getView().getModel("oMainViewModel").setProperty("/customerList", []); 
 
-            // }
 
+
+                if (!this._oCreateCustomerDialog) {
+                    Fragment.load({
+                        id: this.getView().getId(),
+                        name: "com.golive.erbakir.zerbakiregitim.view.fragments.CreateCustomer",
+                        controller: this
+                    }).then(function (oDialog) {
+                        that._oCreateCustomerDialog = oDialog;
+                        that.getView().addDependent(that._oCreateCustomerDialog);
+                        that._oCreateCustomerDialog.open();
+                        //  that.getView().setModel(that.getView().getModel("createCustomerModel"), "createCustomerModel"); // fragment içine veri setleme
+                    });
+                } else {
+                    this._oCreateCustomerDialog.open();
+                }
+            },
+
+            onSaveCustomer: function () {
+                var that = this;
+                var oModel = this.getOwnerComponent().getModel();
+                var oCreateCustomerModel = this.getView().getModel("createCustomerModel");
+                var oData = oCreateCustomerModel.getData();
+
+                // Zorunlu alan kontrolü
+                if (!oData.IvFirstName || oData.IvFirstName.trim() === "") {
+                    MessageBox.error("Ad alanı zorunludur!");
+                    return;
+                }
+
+                //    sap.ui.core.BusyIndicator.show(0);
+                // Busy indicator'ı başlat
+
+                this.getView().getModel("oMainViewModel").setProperty("/busy", true);
+
+                // OData servisine POST isteği gönder
+                // this.oDataModel.create("/CreateCustomerSet", oData, {
+                oModel.create("/CreateCustomerSet", oData, {
+                    success: function (oResponseData, oResponse) {
+                        that.getView().getModel("oMainViewModel").setProperty("/busy", false);
+                        // sap.ui.core.BusyIndicator.hide(0);
+                        // Response'da hata mesajları var mı kontrol et
+                        if (oResponseData.Type === "E") {
+                            MessageBox.error("Müşteri oluştururken hata: " + oResponseData.Message);
+                        } else {
+                            MessageToast.show("Müşteri başarıyla oluşturuldu!");
+                            that._oCreateCustomerDialog.close();
+                            // Listeyi yenile
+                            that.onGetCustomerList();
+                        }
+                    },
+                    error: function (oError) {
+                        that.getView().getModel("oMainViewModel").setProperty("/busy", false);
+
+                        // sap.ui.core.BusyIndicator.hide(0);
+                        var sErrorMessage = "Müşteri oluştururken hata oluştu!";
+                        try {
+                            var oErrorResponse = JSON.parse(oError.responseText);
+                            if (oErrorResponse.error && oErrorResponse.error.message) {
+                                sErrorMessage = oErrorResponse.error.message.value;
+                            }
+                        } catch (e) {
+                            // JSON parse hatası durumunda default mesajı kullan
+                        }
+
+                        MessageBox.error(sErrorMessage);
+                    }
+                });
+            },
+
+            onCancelCreateCustomer: function () {
+                this._oCreateCustomerDialog.close();
+            }
 
         });
     });
